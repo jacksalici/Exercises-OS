feca5f4075f02dd8e8988f0a2cf900ac3ac6fb83
Davide Secco <d.secco2000@gmail.com>
2021-06-02 15:51:48 +0200

    creazione filescheletro + correzione tipico.c

---
+++ C_Esercitazioni/filescheletro.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <ctype.h>
+
+typedef int pipe_t[2];
+
+typedef struct{
+    			/*  */
+	 			/*  */
+} s_occ;
+
+int main(int argc, char *argv[])
+{
+    int pid;      						/* pid per fork */
+   	int *pid;							/* array di pid */
+
+    int N;   							/* numero di caratteri e quindi numero di processi */
+    int fd;      						/* per open */
+    int i, k;     						/* indici, i per i figli! */
+    int cont;     						/* per conteggio */
+    char c;       						/* per leggere dal file */
+    pipe_t *piped;    					/* array dinamico di pipe */
+    int pidFiglio, status, ritorno;    /* variabili per wait*/
+
+	//CONTROLLI TIPICI
+
+    /* OBBLIGATORIO: numero dei caratteri passati sulla linea di comando */
+    if (argc < )   {
+        printf("Errore nel numero dei parametri\n");
+        exit(1);
+    }
+    
+    N = argc - ;
+
+
+	//ALLOCAZIONE MEMORIA MALLOC
+
+    /* OBBLIGATORIO: allocazione N pipe */
+    if ((piped=(pipe_t *)malloc(N*sizeof(pipe_t))) == NULL)    {
+    	printf("Errore allocazione pipe\n");
+    	exit(3); 
+    }
+
+	/* NON È DETTO SERVA: allocazione array per i pid */
+    if ((pid=(int *)malloc(N*sizeof(int))) == NULL)    {
+    	printf("Errore allocazione pid\n");
+    	exit(4);
+    }
+
+	/* NON È DETTO SERVA:  padre aggancia le due funzioni (scrivi e salta) che useranno i figli alla ricezione dei segnali inviati dal padre */
+    signal(SIGUSR1,scrivi);
+    signal(SIGUSR2,salta);
+
+	//CREAZIONE PIPE 
+
+    /* OBBLIGATORIO: creo N pipe */
+    for (i=0; i < N; i++)
+        if (pipe(piped[i]) < 0)        {
+            printf("Errore nella creazione della pipe\n");
+            exit(1);
+        }
+
+	printf("Sono il processo padre con pid%d e sto per generare %d figli\n", getpid(), N);
+    for (i=0; i < N; i++) {
+		/* OBBLIGATORIO: creazione dei figli */
+        if ((pid = fork()) < 0) {
+            printf ("Errore nella fork\n");
+            exit(1);
+        }
+
+		if (pid == 0) /* figlio */ {
+            printf("Figlio %d con pid %d\n", i, getpid());
+
+			/* OBBLIGATORIO: chiude tutte le pipe che non usa (scegli schema chiusura!) */
+
+			/* OBBLIGATORIO: apre il file */
+            if ((fd = open(argv[], O_RDONLY)) < 0) { 
+                printf("Errore nella apertura del file %s\n", argv[1]);
+                exit(-1);
+            }
+
+			/* eseguo codice figlio e eventuale nipote */
+
+
+			exit(0);
+		}
+	}
+
+	/* padre */
+    
+    /* OBBLIGATORIO: chiude tutte le pipe che non usa */
+
+	/* legge dalle pipe i messaggi o manda segnali?*/
+
+	//ATTESA TERMINAZIONE DEI FIGLI   
+    /* Attesa della terminazione dei figli */
+        
+    for(i=0;i < N;i++) {
+    	pidFiglio = wait(&status);
+        if (pidFiglio < 0){
+            printf("Errore wait\n");
+	        exit(9);
+    	}
+            
+        if ((status & 0xFF) != 0)
+            printf("Figlio con pid %d terminato in modo anomalo\n", pidFiglio);
+        else{
+            ritorno=(int)((status >> 8) & 0xFF);
+            printf("Il  figlio  con  pid=%d  ha  ritornato  %d  (se  255 problemi!)\n", pidFiglio, ritorno);
+        }
+    }
+    exit(0);
+}
